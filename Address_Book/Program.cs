92a4eb8c3533a445cade1741b6e524babc72a130
asmithasatesh <asmithasatesh@gmail.com>
2021-07-03 20:32:30 +0530

    [Asmitha]Store addressBook in a dictionary

--- Address_Book/Program.cs
+++ Address_Book/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Address_Book
@@ -52,11 +53,13 @@ namespace Address_Book
             int i=0;
             Console.WriteLine("-------To Modify-------\nEnter first name of user that needs modification");
             string name = Console.ReadLine();
+
             //Traverse till the desired index
             while( ContactArray[i].firstName!= name)
             {
                 i++;
             }
+
             Console.WriteLine("Enter field to be modified 1.firstName 2.lastName 3.Address 4.city 5.state 6.zip 7.phoneNumber 8.email 9.Delete a contact");
             int ch = Convert.ToInt32(Console.ReadLine());
             switch (ch)
@@ -106,13 +109,15 @@ namespace Address_Book
                     ContactArray = ContactArray.Take(i).Concat(ContactArray.Skip(i + 1)).ToArray();
                     contact--;
                     break;
+                default:
+                    Console.WriteLine("Invalid Option");
+                    break;
             }
             //Display Function
             ContactPerson obj = new ContactPerson();
             obj.Display(ContactArray, contact);
 
         }
-
     }
 
 
@@ -120,13 +125,74 @@ namespace Address_Book
     {
         static void Main(string[] args)
         {
-            //Calls Default constructor
-            AddressBookSystem addressBookSystem = new AddressBookSystem();
-            //Call Method
-            addressBookSystem.CreateContact("Ash", "sat", "Chennai thirumulaivoyal", "chennai", "TN", 243001, 9842905050, "asmithasatesh");
-            addressBookSystem.CreateContact("abcd", "lastabcd", "Chennai thirumulaivoyal", "chennai", "TN", 243001, 9842905050, "asmithasatesh@gmail.com");
-            addressBookSystem.CreateContact("As", "sat", "Chennai thirumulaivoyal", "chennai", "TN", 6000611, 9842905050, "asmithasatesh");
-            addressBookSystem.Modify();
+            //Input an AddressBook name
+            Console.WriteLine("Enter number of AddressBook to create");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int i = 0;
+
+            //Create dictionary to store addressbook
+            IDictionary<string, AddressBookSystem[]> numberNames = new Dictionary<string, AddressBookSystem[]>();
+
+            //Runs till number of addressbook needs to be added
+            while (i < num)
+            {
+                //Get input
+                Console.WriteLine("Enter name of addressBook");
+                string addrBookName = Console.ReadLine();
+
+                //Create object for Class
+                AddressBookSystem addressBookSystem = new AddressBookSystem();
+                Console.WriteLine("Enter number of Contacts to Add");
+                int contacts = Convert.ToInt32(Console.ReadLine());
+
+                //Input contacts values from user
+                while (contacts > 0)
+                {
+                    Console.WriteLine("Enter Firstname ");
+                    string firstname = Console.ReadLine();
+                    Console.WriteLine("Enter Lastname ");
+                    string lastname = Console.ReadLine();
+
+                    Console.WriteLine("Enter Address");
+                    string address = Console.ReadLine();
+
+                    Console.WriteLine("Enter City");
+                    string city = Console.ReadLine();
+
+                    Console.WriteLine("Enter State");
+                    string state = Console.ReadLine();
+
+                    Console.WriteLine("Enter pincode");
+                    int pincode = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Enter PhoneNumber ");
+                    long phone = Convert.ToInt64(Console.ReadLine());
+
+                    Console.WriteLine("Enter Email");
+                    string email = Console.ReadLine();
+
+                    //Call Method
+                    addressBookSystem.CreateContact(firstname, lastname, address, city, state, pincode, phone, email);
+                    contacts--;
+                }
+
+                //Check if any modification needed
+                Console.WriteLine("Do you want to Modify?(Y/N");
+                char ch=Convert.ToChar(Console.ReadLine());
+                if (ch == 'Y')
+                {
+                    addressBookSystem.Modify();
+                }
+
+                //Implements IDictionary<TKey, TValue> interface.
+                numberNames.Add(addrBookName, addressBookSystem.ContactArray);
+                foreach (KeyValuePair<string, AddressBookSystem[]> kvp in numberNames)
+                {
+                    //Console.WriteLine("Key: {0}, Value: {1}", kvp.Key, kvp.Value[0].firstName);              
+                    Console.WriteLine("Key: {0}, Value: {1}", kvp.Key, kvp.Value;
+                }
+                i++;
+            }
         }
         //Display Details
         public void Display(AddressBookSystem[] ContactArray, int N)
